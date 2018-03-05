277fe4c6666384d93e3d00cc09df62cf9e83fe39
MohammadMDSA <samimd.77@gmail.com>
2018-03-05 17:10:43 +0330

    Added StorageCore class

---
+++ WarUp/Core/Storage/StorageCore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarUp.Core.Graphics;
+
+namespace WarUp.Core.Storage
+{
+	class StorageCore
+	{
+		public IEnumerable<IDrawable> GetDrawables()
+		{
+			return null;
+		}
+	}
+}
