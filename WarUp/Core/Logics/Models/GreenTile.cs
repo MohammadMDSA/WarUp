55687e5e0d87731c3ccffd926e0f5248facf6218
MohammadMDSA <samimd.77@gmail.com>
2018-03-05 19:37:21 +0330

    Added render framework

---
+++ WarUp/Core/Logics/Models/GreenTile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Graphics.Canvas;
+
+namespace WarUp.Core.Logics.Models
+{
+	class GreenTile : IFrameworkObject
+	{
+		int x;
+		int y;
+
+		public void Draw(CanvasDrawingSession session)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update()
+		{
+			throw new NotImplementedException();
+		}
+
+		enum Direction
+		{
+			Up = 0,
+			Down,
+			Left,
+			Right
+		}
+	}
+}
