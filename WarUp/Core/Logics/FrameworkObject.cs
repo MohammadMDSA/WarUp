a56a7828731538ab574924ccdb9ccd034b9335e1
MohammadMDSA <samimd.77@gmail.com>
2018-03-20 22:54:25 +0330

    Applied ISelectable and added docs

--- WarUp/Core/Logics/FrameworkObject.cs
+++ WarUp/Core/Logics/FrameworkObject.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Spatial.Euclidean;
 using Microsoft.Graphics.Canvas;
 using WarUp.Core.Graphics;
 
 namespace WarUp.Core.Logics.Models
 {
-    public abstract class FrameworkObject : IUpdatable, IDrawable
+
+	/// <summary>
+	/// Represents a framework object
+	/// </summary>
+    public abstract class FrameworkObject : IUpdatable, IDrawable, ISelectable
     {
+		/// <summary>
+		/// Name of object to access
+		/// </summary>
+        public string Name { get; set; }
+
         public abstract void Draw(CanvasDrawingSession session);
-        public abstract void Update();
+        public abstract Polygon2D GetSelectPolygon();
+		public abstract bool IsAvailable();
+		public abstract bool IsSelected();
+		public abstract bool Select();
+		public abstract bool Unselect();
+		public abstract void Update();
     }
 }
