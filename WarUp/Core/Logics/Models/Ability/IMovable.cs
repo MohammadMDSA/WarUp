433c68355c90bccc41d87df1c2c765ff2bfbc953
Mohammad MDSA <samimd.77@gmail.com>
2018-03-21 00:46:13 +0330

    Mutated IMovable interface with methods

--- WarUp/Core/Logics/Models/Ability/IMovable.cs
+++ WarUp/Core/Logics/Models/Ability/IMovable.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using WarUp.Core.Logics.MapUtils;
 using Windows.UI.Composition;
 
 namespace WarUp.Core.Logics.Models.Ability
 {
+	/// <summary>
+	/// Represents ability of moving
+	/// </summary>
 	interface IMovable : IAbility
 	{
-        void Move();
+		/// <summary>
+		/// Move to a specific location
+		/// </summary>
+		/// <param name="destination">Coodinance of destination</param>
+        void Move(Vector2 destination);
+
+		/// <summary>
+		/// Move to a specific waypoint
+		/// </summary>
+		/// <param name="destination">Waypoint of destination</param>
+        void Move(Waypoint destination);
+
+		/// <summary>
+		/// Move along a waypoint route
+		/// </summary>
+		/// <param name="path">Route of movement</param>
+        void Move(WaypointRoute path);
 
 		float GetSpeed();
 	}
