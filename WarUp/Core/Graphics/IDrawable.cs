fcd923b294d7b7867763940207e726ef122b9ed0
MohammadMDSA <samimd.77@gmail.com>
2018-03-20 20:28:30 +0330

    Refactored interfaces

--- WarUp/Core/Graphics/IDrawable.cs
+++ WarUp/Core/Graphics/IDrawable.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Composition;
 
 namespace WarUp.Core.Graphics
 {
--- WarUp/Core/Logics/Models/Ability/IMovable.cs
+++ WarUp/Core/Logics/Models/Ability/IMovable.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Composition;
 
 namespace WarUp.Core.Logics.Models.Ability
 {
 	interface IMovable : IAbility
 	{
-		void Move();
+        void Move();
 	}
 }
