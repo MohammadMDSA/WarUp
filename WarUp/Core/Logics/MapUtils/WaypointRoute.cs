25e54ecadce06d1ab32523c011930b52e11c7289
Mohammad MDSA <samimd.77@gmail.com>
2018-03-21 01:01:43 +0330

    Added WaypointRoute and WaypointRouteNode class

---
+++ WarUp/Core/Logics/MapUtils/WaypointRoute.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Spatial.Euclidean;
+using Microsoft.Graphics.Canvas;
+
+namespace WarUp.Core.Logics.MapUtils
+{
+	public sealed class WaypointRoute : GameUtil
+	{
+		public override void Draw(CanvasDrawingSession session)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override Polygon2D GetSelectPolygon()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override bool IsAvailable()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override bool IsSelected()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override bool Select()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override bool Unselect()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Update()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	/// <summary>
+	/// Represents a waypoint node in waypoint route
+	/// </summary>
+	class WaypointRouteNode
+	{
+		/// <summary>
+		/// Waypoint being represented by node
+		/// </summary>
+		public Waypoint Waypoint { get; }
+
+		/// <summary>
+		/// Set of this node's neighbours
+		/// </summary>
+		public HashSet<Waypoint> Neighbours { get; }
+
+		public WaypointRouteNode(Waypoint waypoint)
+		{
+			this.Waypoint = waypoint;
+			this.Neighbours = new HashSet<Waypoint>();
+		}
+	}
+
+}
