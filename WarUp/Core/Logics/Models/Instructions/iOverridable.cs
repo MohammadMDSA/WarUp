e8f9e2f9481f202839235933c329ae82d4ca612d
Mohammad MDSA <samimd.77@gmail.com>
2018-03-21 13:54:11 +0330

    Added to check if InstructionSets override eachother

---
+++ WarUp/Core/Logics/Models/Instructions/iOverridable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarUp.Core.Logics.Models.Instructions
+{
+	public interface IOverridable<T>
+	{
+		bool CanOverride(T other);
+	}
+}
