7c89638a253f85b78c7fa9ef7e130e1c04f6f649
Mohammad MDSA <samimd.77@gmail.com>
2018-03-21 17:31:16 +0330

    Added Position setter and handled getting instruction sets

--- WarUp/Core/Logics/Models/GameObject.cs
+++ WarUp/Core/Logics/Models/GameObject.cs
@@ -10,18 +10,52 @@ namespace WarUp.Core.Logics.Models
 {
     public abstract class GameObject : FrameworkObject
     {
-        public Vector2 Position { get; set; }
+		public Vector2 Position { get; protected set; }
 
 		public Queue<InstructionSet> Instructions { get; }
 
+		public GameObject()
+		{
+			Position = new Vector2(0f);
+			Instructions = new Queue<InstructionSet>();
+		}
+
+		public void SetPosition(float x, float y)
+		{
+			SetPosition(new Vector2(x, y));
+		}
+
+		public void SetPosition(Vector2 position)
+		{
+			this.Position = position;
+		}
+
 		public void AddInstructionSet(InstructionSet instructionSet)
 		{
-			for (int i = Instructions.Count - 1; instructionSet.CanOverride(Instructions.ElementAt(i)); i--)
+			for (int i = Instructions.Count - 1; i >= 0 && instructionSet.CanOverride(Instructions.ElementAt(i)); i--)
 			{
 				Instructions.ElementAt(i).Enabled = false;
 			}
 
 			Instructions.Enqueue(instructionSet);
 		}
-    }
+
+		protected bool GetFirstEnableInstruction(out InstructionSet instruction)
+		{
+
+			InstructionSet current;
+			instruction = null;
+			if (!Instructions.TryPeek(out current))
+				return false;
+			while (!current.Enabled)
+			{
+				Instructions.Dequeue();
+				if (!Instructions.TryPeek(out current))
+					return false;
+			}
+
+			instruction = current;
+			return true;
+		}
+	}
 }
