42b8766661d0275614a91d20a4fc48ef7dee90d8
Mohammad MDSA <samimd.77@gmail.com>
2018-03-22 20:21:13 +0430

    Implemented Sizing

--- WarUpGraphicEngine/SwapChainManager.cs
+++ WarUpGraphicEngine/SwapChainManager.cs
@@ -18,18 +18,20 @@ namespace WarUp.GraphicEngine
 		public Size RenderSize { get; set; }
 		public Size DisplaySize { get; private set; }
 		public float DPI { get; private set; }
+		private bool UseCore { get; }
 
 		public SwapChainManager(CoreWindow window, CanvasDevice device)
 		{
 			ConstructorHelper(device);
 			SwapChain = CanvasSwapChain.CreateForCoreWindow(device, window, DPI);
+			UseCore = true;
 		}
 
 		public SwapChainManager(CanvasDevice device)
 		{
 			ConstructorHelper(device);
 			SwapChain = new CanvasSwapChain(device, (float)DisplaySize.Width, (float)DisplaySize.Height, DisplayInformation.GetForCurrentView().LogicalDpi);
-
+			UseCore = false;
 		}
 
 		private void ConstructorHelper(CanvasDevice device)
@@ -43,7 +45,13 @@ namespace WarUp.GraphicEngine
 
 		public void EnsureMatchesWindow()
 		{
+			if (UseCore)
+			{
+				var window = CoreWindow.GetForCurrentThread();
+				var bound = window.Bounds;
+				RenderSize = new Size(bound.Width, bound.Height);
 
+			}
 			if (!SizeEqualsWithTolerance(RenderSize, SwapChain.Size) || DPI != SwapChain.Dpi)
 			{
 				SwapChain.ResizeBuffers((float)RenderSize.Width, (float)RenderSize.Height, DPI);
@@ -52,6 +60,13 @@ namespace WarUp.GraphicEngine
 
 		public void EnsureCurrentBufferMatchesWindow(CanvasRenderTarget[] accumulationBuffers, int currentBuffer)
 		{
+			if(UseCore)
+			{
+				var window = CoreWindow.GetForCurrentThread();
+				var bound = window.Bounds;
+				RenderSize = new Size(bound.Width, bound.Height);
+
+			}
 
 			var buffer = accumulationBuffers[currentBuffer];
 
