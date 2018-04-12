cedd42fec11b411518cdffa7b5886ecca8d37dce
Mohammad MDSA <samimd.77@gmail.com>
2018-04-12 13:55:29 +0430

    Changed base namespace

--- GameCore/Properties/AssemblyInfo.cs
+++ GameCore/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameCore")]
+[assembly: AssemblyTitle("WarUpCore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GameCore")]
+[assembly: AssemblyProduct("WarUpCore")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
