16134a7489e01d850d3745cb15ec4efc067e0e1f
Mohammad MDSA <samimd.77@gmail.com>
2018-04-12 15:10:46 +0430

    Added editor keyboard state manager

---
+++ WarUp/Logic/Editor/Input/Keyboard/EditorKeyboard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using WarUp.Canvases;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+
+namespace WarUp.Logic.Editor.Input.Keyboard
+{
+	public class EditorKeyboard
+	{
+		public EditorCanvas Editor { get; }
+
+		public EditorKeyboard(EditorCanvas editor)
+		{
+			this.Editor = editor;
+		}
+
+		public void KeyDown(UIElement sender, KeyRoutedEventArgs e)
+		{
+
+		}
+
+		public void KeyUp(UIElement sender, KeyRoutedEventArgs e)
+		{
+
+		}
+	}
+}
