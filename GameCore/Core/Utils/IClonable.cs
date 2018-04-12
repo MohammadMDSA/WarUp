967cdd055e90e15b2cc3fd0276b5a258b5ce4cc5
Mohammad MDSA <samimd.77@gmail.com>
2018-04-12 13:17:34 +0430

    Made generic typed IClonable interface

---
+++ GameCore/Core/Utils/IClonable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCore.Core.Utils
+{
+	interface IClonable<T>
+	{
+		T Clone();
+	}
+}
