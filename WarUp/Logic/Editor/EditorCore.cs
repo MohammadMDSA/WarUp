bab97a95a2a9709fe7ca715f3a82d3adbf78ea39
Mohammad MDSA <samimd.77@gmail.com>
2018-04-12 15:11:35 +0430

    Added keyboard field

--- WarUp/Logic/Editor/EditorCore.cs
+++ WarUp/Logic/Editor/EditorCore.cs
@@ -7,13 +7,15 @@ using WarUp.Canvases;
 using WarUp.Logic.Editor.Input.Mouse;
 using WarUp.Logic.Editor.Input;
 using WarUp.Core.Storage;
+using WarUp.Logic.Editor.Input.Keyboard;
 
 namespace WarUp.Logic.Editor
 {
 	class EditorCore
 	{
 		public EditorCanvas EditorCanvas { get; }
-		public EditorMouse EditorMouse { get; }
+		public EditorMouse Mouse { get; }
+		public EditorKeyboard Keyboard { get; }
 		public StorageCore Storage { get; }
 
 		public EditorCore(EditorCanvas editorCanvas)
@@ -21,9 +23,10 @@ namespace WarUp.Logic.Editor
 			this.EditorCanvas = editorCanvas;
 
 			this.Storage = new StorageCore();
-			EditorInput.Mouse = this.EditorMouse = new EditorMouse(Storage, EditorCanvas);
+			EditorInput.Mouse = this.Mouse = new EditorMouse(Storage, EditorCanvas);
+			EditorInput.Keyboard = this.Keyboard = new EditorKeyboard();
 
-			EditorCanvas.Mouse = EditorMouse;
+			EditorCanvas.Mouse = Mouse;
 			EditorCanvas.Storage = Storage;
 		}
 
--- WarUp/Logic/Editor/Input/Input.cs
+++ WarUp/Logic/Editor/Input/Input.cs
@@ -1,9 +1,11 @@
-﻿using WarUp.Logic.Editor.Input.Mouse;
+﻿using WarUp.Logic.Editor.Input.Keyboard;
+using WarUp.Logic.Editor.Input.Mouse;
 
 namespace WarUp.Logic.Editor.Input
 {
 	public static class EditorInput
 	{
 		public static EditorMouse Mouse { get; internal set; }
+		public static EditorKeyboard Keyboard { get; internal set; }
 	}
 }
